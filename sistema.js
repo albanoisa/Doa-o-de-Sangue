dd5e78b16b3f345a818d1e1672605941b63da0b6
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-04-13 12:22:26 -0300

    add - js finalizado

--- sistema.js
+++ sistema.js
@@ -1,13 +1,22 @@
-document.getElementById(formDoacao) .addEventListener("submit", function (e) {
-    e.preventDeFault();
+document.getElementById("formDoacao").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
-    let idade = document.getElementById("idade").value;
-    let peso = document.getElementById("peso").value;
+    let nome = document.getElementById("nome").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let idade = document.getElementById("idade").value.trim();
+    let peso = document.getElementById("peso").value.trim();
     let tipo_sanguineo = document.getElementById("tipo sanguineo").value;
-    let telefone = document.getElementById("telefone").value;
-    let cidade = document.getElementById("cidade").value;
-    let estado = document.getElementById("estado").value;
-}
-)
+    let telefone = document.getElementById("telefone").value.trim();
+    let cidade = document.getElementById("cidade").value.trim();
+    let estado = document.getElementById("estado").value.trim();
+
+    if(nome.length < 2 ) return alert("Digite nome e sobrenome");
+    if(idade < 16 ) return alert("Idade deve ser superior a 16 anos");
+    if(peso < 50) return alert("Peso deve ser superior a 50kg");
+    if (nome || email || !idade || peso || tipo_sanguineo || telefone || cidade || estado)  return alert("Todos os campos são obrigatórios");
+    if (tipo_sanguineo === "")  return alert("Selecione o tipo sanguíneo");
+    
+    alert("Formulário enviado com sucesso!");
+    
+   
+});
